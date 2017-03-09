5e3f505ad12eb165576dfde3ab15a936f8aa4930
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-09 00:36:57 -0800

    Export cart reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import CartReducer from './CartReducer';
 
 export default combineReducers({
-	auth: AuthReducer
+	auth: AuthReducer,
+	cart: CartReducer
 });
